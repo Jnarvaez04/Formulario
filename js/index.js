26e2d37d94d966059e80ec4781177b7d8ec07280
jnarvaez04 <j.narvaez564@gmail.com>
2021-11-17 21:12:26 -0500

    Intento fallido de js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
-let valTextBox = document.getElementById('textBox').value; 
-console.log(valTextBox);
+const Formulario = document.getElementById ("form");
+Formulario.addEventListener ('submit',e=>{
+    e.preventDefault();
+    const nombres = Formulario['nombre'].value;
+    console.log(nombre);
+})
